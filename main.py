9bf9ca337638ac247fe33ba0aa552538ccc98862
Vlad <vdubovoy425@gmail.com>
2025-04-23 18:16:29 +0300

    Minor fix

--- main.py
+++ main.py
@@ -20,10 +20,15 @@ if __name__ == "__main__":
         audio_path = record_audio()
         text = transcribe_audio(audio_path)
         conversation.append({"role": "user", "content": text + "Answer in few sentences."})
+
         print(f'You said: {text}\n\n')
         print("Waiting AI response...\n\n")
+
         response = get_response(conversation)
         conversation.append({"role": "assistant", "content": response})
+
         print(f'AI response: {response}\n\n')
+
         speaker(response)
+        
         print("Press Ctrl+C to stop conversation\n\n")
